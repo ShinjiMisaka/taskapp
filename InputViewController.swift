d34732a9e1d585f9d4055bb3d3066ebefbd9a14e
shinji.misaka <shinmisa0114@gmail.com>
2019-07-05 20:08:18 +0900

    タスク管理の機能を完成させた。

---
+++ InputViewController.swift
@@ -0,0 +1,48 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 三坂真治 on 2019/07/05.
+//  Copyright © 2019 shinji.misaka. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class InputViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!    
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    let realm = try! Realm()
+    var task: Task!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date
+            self.realm.add(self.task, update: true)
+        }
+        
+        super.viewWillDisappear(animated)
+    }
+    
+    @objc func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
+}
